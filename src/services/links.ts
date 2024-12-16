e997049382d4abb41df971890488b0ffb8b4ec31
aiwaki <aivaki@vk.com>
2024-12-16 19:15:50 +0300

    Add links.ts

---
+++ src/services/links.ts
@@ -0,0 +1,28 @@
+import { API } from "../utils/api";
+import { type SingleResponse } from "../utils/types";
+import { type Report } from "./reports";
+import { request } from "./base-api";
+
+export interface CreateLinkRequestParams {
+  url: string;
+}
+
+export interface CreateLinkResponse {
+  link: any;
+  count: number;
+  updatedAt: string;
+  report: Report;
+}
+
+export const createLink = async (
+  params: CreateLinkRequestParams, tg_id: number
+): Promise<SingleResponse<CreateLinkResponse>> => {
+  const { data } = await request<SingleResponse<CreateLinkResponse>>({
+    url: `${API.link}/create`,
+    method: "POST",
+    data: params,
+    tg_id: tg_id
+  });
+
+  return data;
+};
