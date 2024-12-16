b25c6716d78377d6494ed66bb036d55d50eb182d
aiwaki <aivaki@vk.com>
2024-12-16 19:15:51 +0300

    Add media.ts

---
+++ src/services/media.ts
@@ -0,0 +1,31 @@
+import { API } from "../utils/api";
+import { type SingleResponse } from "../utils/types";
+import { request } from "./base-api";
+
+export enum Media {
+  PNG = "image/png",
+  JPG = "image/jpeg",
+}
+
+export interface CreateMediaRequestParams {
+  format: Media;
+}
+
+export interface CreateMediaResponse {
+  id: string;
+  upload: string;
+  fileUrl: string;
+}
+
+export const createMedia = async (
+  params: CreateMediaRequestParams, tg_id: number
+): Promise<SingleResponse<CreateMediaResponse>> => {
+  const { data } = await request<SingleResponse<CreateMediaResponse>>({
+    url: `${API.media}/upload`,
+    method: "POST",
+    data: params,
+    tg_id: tg_id
+  });
+
+  return data;
+};
