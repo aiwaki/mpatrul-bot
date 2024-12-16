4a9149eb79712163c581bf1167a95be535b949fc
aiwaki <aivaki@vk.com>
2024-12-16 19:15:50 +0300

    Add identity.ts

---
+++ src/services/identity.ts
@@ -0,0 +1,28 @@
+import { type SingleResponse } from "../utils/types";
+import { type Profile } from "./profiles";
+import { request } from "./base-api";
+import { API } from "../utils/api";
+
+export interface SignInRequestParams {
+  login: string;
+  password: string;
+}
+
+export interface LoginResponseParams {
+  profile: Profile;
+  accessToken: string;
+  expiresAt: number;
+}
+
+export const signIn = async (
+  params: SignInRequestParams, tg_id: number
+): Promise<SingleResponse<LoginResponseParams>> => {
+  const { data } = await request<SingleResponse<LoginResponseParams>>({
+    url: `${API.identity}/sign-in`,
+    method: "POST",
+    data: params,
+    tg_id: tg_id
+  });
+
+  return data;
+};
