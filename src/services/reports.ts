559b808f7a06cc0c2e8e6a6d70262f0bc163de4b
aiwaki <aivaki@vk.com>
2024-12-16 19:15:52 +0300

    Add reports.ts

---
+++ src/services/reports.ts
@@ -0,0 +1,40 @@
+import { API } from "../utils/api";
+import { type SingleResponse } from "../utils/types";
+import { request } from "./base-api";
+
+export interface CreateReportRequestParams {
+  url: string;
+  content: string;
+  isPersonal: boolean;
+  isMedia: boolean;
+  desciption: string;
+  photoId: string;
+}
+
+export interface Report {
+  id: string;
+  url: string;
+  createdAt: string;
+  content: string;
+  isPersonal: string;
+  desciption: string;
+  author: {
+    name: string;
+  };
+  photo: {
+    url: string;
+  };
+}
+
+export const createReport = async (
+  params: CreateReportRequestParams, tg_id: number
+): Promise<SingleResponse<Report>> => {
+  const { data } = await request<SingleResponse<Report>>({
+    url: `${API.report}/create`,
+    method: "POST",
+    data: params,
+    tg_id: tg_id
+  });
+
+  return data;
+};
