2a5d02dac71810f1fd4153a1e08177bd796b3235
aiwaki <aivaki@vk.com>
2024-12-18 16:44:19 +0300

    Update reports.ts

--- src/services/reports.ts
+++ src/services/reports.ts
@@ -1,6 +1,11 @@
+import { screenshotPage } from "../browser/pages";
+import { insertLink } from "../database/api";
 import { API } from "../utils/api";
+import { ReportType } from "../utils/constants";
 import { type SingleResponse } from "../utils/types";
+import { uploadFile } from "../utils/uploadFile";
 import { request } from "./base-api";
+import { type CreateMediaRequestParams, Media, createMedia } from "./media";
 
 export interface CreateReportRequestParams {
   url: string;
@@ -36,4 +41,34 @@ export const createReport = async (
   }, tg_id);
 
   return data;
+};
+
+export const createReportWithAi = async (): Promise<void> => {
+  const alreadyExists = await insertLink(chatId, url)
+  if (alreadyExists) {
+    await ctx.sendChatAction('typing');
+    await ctx.reply('👌 Ссылка уже существует.');
+    await ctx.scene.leave();
+  }
+
+  const screenshot = await screenshotPage(url)
+
+  const mediaParams: CreateMediaRequestParams = {
+    format: Media.PNG
+  }
+  const mediaResponse = await createMedia(mediaParams, chatId)
+  if (mediaResponse.error) throw new Error(mediaResponse.error);
+
+  await uploadFile(mediaResponse.data.upload, screenshot)
+
+  const createRequestParams: CreateReportRequestParams = {
+    url,
+    content: ReportType.Propaganda.toString(),
+    isPersonal: true,
+    isMedia: false,
+    desciption: "Продажа семян.",
+    photoId: mediaResponse.data.id,
+  }
+  const reportResponse = await createReport(createRequestParams, chatId);
+  if (reportResponse.error) throw new Error(reportResponse.error);
 };
