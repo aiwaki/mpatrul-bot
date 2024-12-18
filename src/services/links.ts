b8e5647882a557a0408cf313d8945ddf788e0b52
aiwaki <aivaki@vk.com>
2024-12-18 14:36:31 +0300

    Update links.ts

--- src/services/links.ts
+++ src/services/links.ts
@@ -20,9 +20,8 @@ export const createLink = async (
   const { data } = await request<SingleResponse<CreateLinkResponse>>({
     url: `${API.link}/create`,
     method: "POST",
-    data: params,
-    tg_id: tg_id
-  });
+    data: params
+  }, tg_id);
 
   return data;
 };
