528a735b09287e3f05af287de93b547fb0d2a013
aiwaki <aivaki@vk.com>
2024-12-21 23:14:37 +0300

    Update routes.ts

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,8 +1,3 @@
 export const ROUTE_SIGN_IN = "/sign-in";
-export const ROUTE_SIGN_UP = "/sign-up";
 export const ROUTE_PROFILE = "/profile";
-export const ROUTE_VOLUNTEERS = "/volunteers";
 export const ROUTE_REPORTS = "/reports";
-export const ROUTE_REPORTS_SQUAD = "/reports/squad";
-export const ROUTE_UPDATES = "/updates";
-export const ROUTE_STATISTICS = "/statistics";
