54928a5f9f6cd42bee2a9ef1ebc0d6a34ed6f1ea
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:20 +0300

    Update commands.ts

--- src/telegram/commands.ts
+++ src/telegram/commands.ts
@@ -1,32 +1,32 @@
-import { bot } from './client'
+import { bot } from "./client";
 
 bot.telegram.setMyCommands([
-    {
-        command: '/start',
-        description: 'Новый диалог',
-    },
-    {
-        command: '/signin',
-        description: 'Вход в систему',
-    },
-    {
-        command: '/me',
-        description: 'Карточка волонтера',
-    },
-    {
-        command: '/link',
-        description: 'Проверить ссылку и отправить отчет',
-    },
-    {
-        command: '/verify',
-        description: 'Подтвердить отправку отчета',
-    }
-])
+  {
+    command: "/start",
+    description: "Новый диалог",
+  },
+  {
+    command: "/signin",
+    description: "Вход в систему",
+  },
+  {
+    command: "/me",
+    description: "Карточка волонтера",
+  },
+  {
+    command: "/link",
+    description: "Проверить ссылку и отправить отчет",
+  },
+  {
+    command: "/verify",
+    description: "Проверить предложенную страницу",
+  },
+]);
 
 const commands = ["signin", "me", "link", "verify"];
 
 commands.forEach((command) => {
-    bot.command(command, async (ctx) => {
-        await ctx.scene.enter(command);
-    });
+  bot.command(command, async (ctx) => {
+    await ctx.scene.enter(command);
+  });
 });
