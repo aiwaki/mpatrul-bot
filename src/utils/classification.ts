644338bce5c9fa38694ac9e3c6ee647020b55548
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:24 +0300

    Update classification.ts

--- src/utils/classification.ts
+++ src/utils/classification.ts
@@ -1,34 +1,37 @@
-import { HfInference, type ZeroShotClassificationOutput } from '@huggingface/inference';
-import { REPORTS, CONTEXTS, type ReportType } from './constants';
+import {
+  HfInference,
+  type ZeroShotClassificationOutput,
+} from "@huggingface/inference";
+import { REPORTS, CONTEXTS, type ReportType } from "./constants";
 
 const hf = new HfInference(process.env.HUGGING_FACE_TOKEN as string);
 
 const zeroShotClassification = async (
-    text: string,
-    candidateLabels: string[]
+  text: string,
+  candidateLabels: string[]
 ): Promise<ZeroShotClassificationOutput> =>
-    await hf.zeroShotClassification({
-        model: 'cointegrated/rubert-base-cased-nli-threeway',
-        inputs: text,
-        parameters: { candidate_labels: candidateLabels },
-    });
+  await hf.zeroShotClassification({
+    model: "cointegrated/rubert-base-cased-nli-threeway",
+    inputs: text,
+    parameters: { candidate_labels: candidateLabels },
+  });
 
 export interface ClassificationOutput {
-    label: string;
-    content: ReportType;
-    score: number;
+  label: string;
+  content: ReportType;
+  score: number;
 }
 
 export const classifyText = async (
-    text: string
+  text: string
 ): Promise<ClassificationOutput> => {
-    const out = (await zeroShotClassification(text.toLowerCase(), REPORTS))[0];
-    const maxIndex = out.scores.indexOf(Math.max(...out.scores));
-    const label = out.labels[maxIndex]
+  const out = (await zeroShotClassification(text.toLowerCase(), REPORTS))[0];
+  const maxIndex = out.scores.indexOf(Math.max(...out.scores));
+  const label = out.labels[maxIndex];
 
-    return {
-        label,
-        content: CONTEXTS[label],
-        score: out.scores[maxIndex]
-    };
+  return {
+    label,
+    content: CONTEXTS[label],
+    score: out.scores[maxIndex],
+  };
 };
