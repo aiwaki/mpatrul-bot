c62d96b68144137368836b55aaaee969d5937c60
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:13 +0300

    Update browser.ts

--- src/browser/browser.ts
+++ src/browser/browser.ts
@@ -5,33 +5,31 @@ import { connect } from "puppeteer-real-browser";
 let browser: Browser | null = null;
 
 export const getBrowserInstance = async (): Promise<Browser> => {
-    if (!browser) {
-        const connectionResult = await connect({
-            args: [
-                "--start-maximized",
-                '--disable-web-security',
-                '--disable-features=SafeBrowsing',
-            ],
-            turnstile: true,
-            headless: false,
-            connectOption: {
-                defaultViewport: null
-            },
-            plugins: [
-                AdblockerPlugin({ blockTrackersAndAnnoyances: true })
-            ]
-        });
-        browser = connectionResult.browser;
-        console.log("Browser instance created.");
-    }
-    return browser;
-}
+  if (!browser) {
+    const connectionResult = await connect({
+      args: [
+        "--start-maximized",
+        "--disable-web-security",
+        "--disable-features=SafeBrowsing",
+      ],
+      turnstile: true,
+      headless: false,
+      connectOption: {
+        defaultViewport: null,
+      },
+      plugins: [AdblockerPlugin({ blockTrackersAndAnnoyances: true })],
+    });
+    browser = connectionResult.browser;
+    console.log("Browser instance created.");
+  }
+  return browser;
+};
 
 const closeBrowser = async () => {
-    if (browser) {
-        await browser.close();
-        console.log("Browser instance closed.");
-    }
+  if (browser) {
+    await browser.close();
+    console.log("Browser instance closed.");
+  }
 };
 
 process.on("SIGINT", closeBrowser);
