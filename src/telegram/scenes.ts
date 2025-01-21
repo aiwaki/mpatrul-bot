6e2fad9099aa82e7821eef1e82d07f9ad2ba9f64
aiwaki <aiwaki@inbox.ru>
2025-01-21 23:55:15 +0300

    Update scenes.ts

--- src/telegram/scenes.ts
+++ src/telegram/scenes.ts
@@ -3,8 +3,9 @@ import { Scenes, session } from "telegraf";
 import { linkWizard } from './wizards/link'
 import { meWizard } from './wizards/me'
 import { signinWizard } from './wizards/signin';
+import { verifyWizard } from './wizards/verify';
 
-const stage = new Scenes.Stage<Scenes.WizardContext>([linkWizard, meWizard, signinWizard]);
+const stage = new Scenes.Stage<Scenes.WizardContext>([linkWizard, meWizard, signinWizard, verifyWizard]);
 
 bot.use(session());
 bot.use(stage.middleware());
