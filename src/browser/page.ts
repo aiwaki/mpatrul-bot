0a3442cb50aa6d6b0f76f10b55e4140a910669a9
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:14 +0300

    Update page.ts

--- src/browser/page.ts
+++ src/browser/page.ts
@@ -1,83 +1,100 @@
-import { Page as PuppeteerPage } from "rebrowser-puppeteer-core";;
-import { classifyText, type ClassificationOutput } from '../utils/classification';
-import { getBrowserInstance } from './browser';
+import { Page as PuppeteerPage } from "rebrowser-puppeteer-core";
+import {
+  classifyText,
+  type ClassificationOutput,
+} from "../utils/classification";
+import { getBrowserInstance } from "./browser";
 
 export interface ExtendedPage extends PuppeteerPage {
-    screenshotFile(): Promise<File>;
-    pageInfo(): Promise<PageInfo>;
+  screenshotFile(): Promise<File>;
+  pageInfo(): Promise<PageInfo>;
 }
 
 export interface PageInfo {
-    title: string;
-    description: string;
-    url: string;
-    classifyOut: ClassificationOutput;
+  title: string;
+  description: string;
+  url: string;
+  classifyOut: ClassificationOutput;
 }
 
 export const extendPage = (page: PuppeteerPage): ExtendedPage => {
-    (page as ExtendedPage).screenshotFile = async function (): Promise<File> {
-        const screenshotData = await this.screenshot();
-        return new File([new Uint8Array(screenshotData)], "screenshot.png", { type: "image/png" });
+  (page as ExtendedPage).screenshotFile = async function (): Promise<File> {
+    const screenshotData = await this.screenshot();
+    return new File([new Uint8Array(screenshotData)], "screenshot.png", {
+      type: "image/png",
+    });
+  };
+
+  (page as ExtendedPage).pageInfo = async function (): Promise<PageInfo> {
+    const url = this.url();
+    const title = await this.title();
+    const description =
+      (await this.$eval("meta[name='description']", (el) =>
+        el.getAttribute("content")
+      ).catch(() => null)) || "Описание не найдено.";
+    const classifyOut = await getBestClassification(title, description);
+
+    return {
+      url,
+      title,
+      description,
+      classifyOut,
     };
+  };
 
-    (page as ExtendedPage).pageInfo = async function (): Promise<PageInfo> {
-        const url = this.url();
-        const title = await this.title();
-        const description = await this.$eval(
-            "meta[name='description']",
-            (el) => el.getAttribute("content")
-        ).catch(() => null) || "Описание не найдено.";
-        const classifyOut = await getBestClassification(title, description);
-
-        return {
-            url,
-            title,
-            description,
-            classifyOut
-        };
-    };
-
-    return page as ExtendedPage;
+  return page as ExtendedPage;
 };
 
 const getBestClassification = async (title: string, description: string) => {
-    const titleResult = await classifyText(title);
-    if (titleResult.score >= 0.4) {
-        return titleResult;
-    }
+  const titleResult = await classifyText(title);
+  if (titleResult.score >= 0.4) {
+    return titleResult;
+  }
 
-    const descriptionResult = await classifyText(description);
-    return descriptionResult.score > titleResult.score ? descriptionResult : titleResult;
+  const descriptionResult = await classifyText(description);
+  return descriptionResult.score > titleResult.score
+    ? descriptionResult
+    : titleResult;
 };
 
-export async function getPageScreenshot(url: string): Promise<File | undefined> {
-    const browser = await getBrowserInstance();
-    const page = extendPage(await browser.newPage());
+export async function getPageScreenshot(
+  url: string
+): Promise<File | undefined> {
+  const browser = await getBrowserInstance();
+  const page = extendPage(await browser.newPage());
 
-    try {
-        console.log(`Screenshoting page for URL: ${url}`);
-        await page.goto(url, { referer: "https://ya.ru/", waitUntil: "domcontentloaded", timeout: 0 });
-        return await page.screenshotFile();
-    } catch (error) {
-        console.error("Error screenshoting page:", error);
-        return undefined;
-    } finally {
-        await page.close();
-    }
+  try {
+    console.log(`Screenshoting page for URL: ${url}`);
+    await page.goto(url, {
+      referer: "https://ya.ru/",
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    return await page.screenshotFile();
+  } catch (error) {
+    console.error("Error screenshoting page:", error);
+    return undefined;
+  } finally {
+    await page.close();
+  }
 }
 
 export async function getPageInfo(url: string): Promise<PageInfo | undefined> {
-    const browser = await getBrowserInstance();
-    const page = extendPage(await browser.newPage());
+  const browser = await getBrowserInstance();
+  const page = extendPage(await browser.newPage());
 
-    try {
-        console.log(`Fetching page info for URL: ${url}`);
-        await page.goto(url, { referer: "https://ya.ru/", waitUntil: "domcontentloaded", timeout: 0 });
-        return await page.pageInfo();
-    } catch (error) {
-        console.error("Error processing page:", error);
-        return undefined;
-    } finally {
-        await page.close();
-    }
+  try {
+    console.log(`Fetching page info for URL: ${url}`);
+    await page.goto(url, {
+      referer: "https://ya.ru/",
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    return await page.pageInfo();
+  } catch (error) {
+    console.error("Error processing page:", error);
+    return undefined;
+  } finally {
+    await page.close();
+  }
 }
