0833ba073a97173e56784e23e8d3faa8f159ef76
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:22 +0300

    Update me.ts

--- src/telegram/wizards/me.ts
+++ src/telegram/wizards/me.ts
@@ -1,51 +1,51 @@
-import { Scenes } from 'telegraf';
-import { fmt, bold } from 'telegraf/format';
-import { getMyProfile } from '../../services/profiles';
-import { fetchToken } from '../../database/chats';
+import { Scenes } from "telegraf";
+import { fmt, bold } from "telegraf/format";
+import { getMyProfile } from "../../services/profiles";
+import { fetchToken } from "../../database/chats";
 
 export const meWizard = new Scenes.WizardScene<Scenes.WizardContext>(
-    'me',
-    async (ctx) => {
-        const chatId = ctx.chat?.id;
-        if (!chatId) {
-            await ctx.sendChatAction('typing');
-            await ctx.reply('⚠️ Не удалось определить идентификатор чата.');
-            return ctx.scene.leave();
-        }
+  "me",
+  async (ctx) => {
+    const chatId = ctx.chat?.id;
+    if (!chatId) {
+      await ctx.sendChatAction("typing");
+      await ctx.reply("⚠️ Не удалось определить идентификатор чата.");
+      return ctx.scene.leave();
+    }
 
-        try {
-            const token = await fetchToken(chatId)
-            if (!token) {
-                await ctx.sendChatAction('typing');
-                await ctx.reply('⚠️ Войдите в аккаунт.');
-                return ctx.scene.leave();
-            }
+    try {
+      const token = await fetchToken(chatId);
+      if (!token) {
+        await ctx.sendChatAction("typing");
+        await ctx.reply("⚠️ Войдите в аккаунт.");
+        return ctx.scene.leave();
+      }
 
-            const response = await getMyProfile(undefined, chatId);
-            if (response.error) {
-                throw new Error(response.error);
-            }
-            const { data } = response;
+      const response = await getMyProfile(undefined, chatId);
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      const { data } = response;
 
-            const message = fmt`
-📝 ${bold('Карточка волонтера')}
+      const message = fmt`
+📝 ${bold("Карточка волонтера")}
 
-👤 ${bold('Имя')}: ${data.name || 'Не указано'}
-📅 ${bold('Дата создания')}: ${new Date(data.createdAt).toLocaleString()}
-✔️ ${bold('Верифицирован')}: ${data.isVerify ? 'Да' : 'Нет'}
-💼 ${bold('Опыт')}: ${data.experience}
-🏢 ${bold('Отряд')}: ${data.parent?.name || 'Не указано'}
+👤 ${bold("Имя")}: ${data.name || "Не указано"}
+📅 ${bold("Дата создания")}: ${new Date(data.createdAt).toLocaleString()}
+✔️ ${bold("Верифицирован")}: ${data.isVerify ? "Да" : "Нет"}
+💼 ${bold("Опыт")}: ${data.experience}
+🏢 ${bold("Отряд")}: ${data.parent?.name || "Не указано"}
             `;
 
-            await ctx.sendChatAction('typing');
-            await ctx.reply(message);
-        } catch (error) {
-            console.error('🚨 Ошибка при получении карточки волонтера:', error)
+      await ctx.sendChatAction("typing");
+      await ctx.reply(message);
+    } catch (error) {
+      console.error("🚨 Ошибка при получении карточки волонтера:", error);
 
-            await ctx.sendChatAction('typing');
-            await ctx.reply('🚨 Произошла ошибка при получении данных.');
-        }
-
-        return ctx.scene.leave();
+      await ctx.sendChatAction("typing");
+      await ctx.reply("🚨 Произошла ошибка при получении данных.");
     }
+
+    return ctx.scene.leave();
+  }
 );
