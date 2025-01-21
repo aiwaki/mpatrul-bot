363f51cdd31f61507b941ccda6741f1b05b99ac4
aiwaki <aiwaki@inbox.ru>
2025-01-22 01:14:21 +0300

    Update start.ts

--- src/telegram/start.ts
+++ src/telegram/start.ts
@@ -1,14 +1,14 @@
-import { insertChat } from '../database/chats';
-import { bot } from './client';
-import { fmt, bold } from 'telegraf/format';
+import { insertChat } from "../database/chats";
+import { bot } from "./client";
+import { fmt, bold } from "telegraf/format";
 
 bot.start(async (ctx) => {
-    await insertChat(ctx.chat);
-    await ctx.reply(
-        fmt`
-🎉 ${bold('Добро пожаловать!')}
+  await insertChat(ctx.chat);
+  await ctx.reply(
+    fmt`
+🎉 ${bold("Добро пожаловать!")}
 
 🚀 Вы готовы начать? Используйте команды, чтобы взаимодействовать с ботом.
         `
-    );
+  );
 });
