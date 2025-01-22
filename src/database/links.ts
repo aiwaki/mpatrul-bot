9e5509bcb7fe5978098479e9fd28839b6b81b408
aiwaki <aiwaki@inbox.ru>
2025-01-22 22:41:30 +0300

    Update links.ts

--- src/database/links.ts
+++ src/database/links.ts
@@ -33,3 +33,42 @@ export const insertLink = async (tg_id: number, url: string): Promise<void> => {
     console.error("Unexpected error in insertLinks:", error);
   }
 };
+
+export const getLinkStats = async (
+  period: "all" | "day" | "week" | "month"
+): Promise<{ count: number }> => {
+  try {
+    let query = supabase.from("links").select("id", { count: "exact" });
+
+    switch (period) {
+      case "day":
+        query = query.gte(
+          "created_at",
+          new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString()
+        );
+        break;
+      case "week":
+        query = query.gte(
+          "created_at",
+          new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString()
+        );
+        break;
+      case "month":
+        query = query.gte(
+          "created_at",
+          new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString()
+        );
+        break;
+    }
+
+    const { count, error } = await query;
+    if (error) {
+      handleError("Error fetching link statistics:", error);
+      return { count: 0 };
+    }
+    return { count: count || 0 };
+  } catch (error) {
+    console.error("Unexpected error in getLinkStats:", error);
+    return { count: 0 };
+  }
+};
